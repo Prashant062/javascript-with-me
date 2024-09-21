3421ce500fa7383e9e626ee22099c96dcf49e558
Prashant062 <prashantrawat786216@gmail.com>
2024-09-21 22:04:23 +0530

    Revision

--- iterations_05/three.js
+++ iterations_05/three.js
@@ -6,12 +6,12 @@
 const arr = [1, 2, 3, 4, 5]
 
 for (const num of arr) {
-    //console.log(num);
+    // console.log(num);
 }
 
 const greetings = "Hello world!"
 for (const greet of greetings) {
-    //console.log(`Each char is ${greet}`)
+    // console.log(`Each char is ${greet}`)
 }
 
 // Maps
@@ -20,13 +20,13 @@ const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
-map.set('IN', "India")
+// map.set('IN', "India")
 
 
-// console.log(map);
+console.log(map);
 
 for (const [key, value] of map) {
-    // console.log(key, ':-', value);
+    console.log(key, ':-', value);
 }
 
 const myObject = {
--- iterations_05/two.js
+++ iterations_05/two.js
@@ -9,11 +9,11 @@ let myArray = ['flash', "batman", "superman"]
 
 let arr = 0
 while (arr < myArray.length) {
-    //console.log(`Value is ${myArray[arr]}`);
+    // console.log(`Value is ${myArray[arr]}`);
     arr = arr + 1
 }
 
-let score = 11
+let score = 1
 
 do {
     console.log(`Score is ${score}`);
